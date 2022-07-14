8bfa302c1a8aaae9cff97e8d86edfe833deb26d5
sabanekko3 <tetayaae@gmail.com>
2022-07-14 17:10:16 +0900

    change func name

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,7 @@
 #include"..\lib\FlexCAN_T4-master\FlexCAN_T4.h"
 void can_setup(void);
 void can_tx(uint32_t id,uint8_t* tx_data);
-void can_rx();
-void canSniff(const CAN_message_t &msg);
+void can_rx(const CAN_message_t &msg);
 
 FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>*my_can1 = new FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>();
 
@@ -26,7 +25,7 @@ void can_setup(void){
     my_can1->setMaxMB(16);
     my_can1->enableFIFO();
     my_can1->enableFIFOInterrupt();
-    my_can1->onReceive(MB0, canSniff);
+    my_can1->onReceive(MB0, can_rx);
     my_can1->mailboxStatus();
     //CAN message config
 }
@@ -40,6 +39,6 @@ void can_tx(uint32_t id,uint8_t* tx_data){
     my_can1->write(*can_msg);
 }
 
-void canSniff(const CAN_message_t &msg){
+void can_rx(const CAN_message_t &msg){
 
 }
